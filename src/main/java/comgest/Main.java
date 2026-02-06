0b8696cef903887bef3140e730b34f9de4d1f7f4
Leonardo Espínola <lespinolafranco@gmail.com>
2026-02-06 19:49:17 -0400

    añadir saldo al usuario y arreglar el path

--- src/main/java/comgest/Main.java
+++ src/main/java/comgest/Main.java
@@ -5,7 +5,7 @@ import comgest.model.ControladorDB;
 public class Main {
     public static void main(String[] args) {
         ControladorDB controladorDB = new ControladorDB(); //controlador de la base de datos de usuarios
-        controladorDB.RegistrarUsuario("leonardo", "Palmpilot2?", "lespinolafranco@gmail.com");
+        //controladorDB.RegistrarUsuario("leonardo", "Palmpilot2?", "lespinolafranco@gmail.com");
         controladorDB.InicioDeSesion("lespinolafranco@gmail.com", "Palmpilot2?");
     }
 }
