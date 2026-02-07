101b7354c2153a3794729b7b04a777ce73a9e742
Leonardo Espínola <lespinolafranco@gmail.com>
2026-02-06 21:36:36 -0400

    merge de 2 mains

--- src/main/java/comgest/Main.java
+++ src/main/java/comgest/Main.java
@@ -5,8 +5,35 @@ import comgest.model.ControladorMenuDB;
 import comgest.model.MenuItem;
 import java.util.List;
 
+import javax.swing.*;
+import comgest.view.components.Panel_Inferior_PUI;
+import comgest.view.components.panel_menu_UI;
+import java.awt.*;
+
+
 public class Main {
     public static void main(String[] args) {
+        DbControlador();
+        IGUControlador();
+    }
+
+    public static void IGUControlador(){
+        JFrame vp = new JFrame();
+
+        vp.setSize(800, 600);
+        vp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Panel_Inferior_PUI panel = new Panel_Inferior_PUI();
+        vp.add(panel.getPanel_inf(), BorderLayout.SOUTH);
+        panel_menu_UI panel_menu = new panel_menu_UI();
+        vp.add(panel_menu, BorderLayout.CENTER);
+        vp.revalidate();
+        vp.repaint();
+        vp.setVisible(true);
+        vp.setResizable(false);
+    }
+
+    public static void DbControlador(){
         ControladorDB controladorDB = new ControladorDB(); // controlador de la base de datos de usuarios
         controladorDB.RegistrarUsuario("julian alvarez", "pollitacock", "c", 0);
 
